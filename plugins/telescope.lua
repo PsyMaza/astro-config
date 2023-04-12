2daaafaad68be2ed223a58b254bae35e8d91cfae
Евгений <psymazagmail.com@MacBook-Pro-Evgenij-7.local>
2023-04-12 22:36:25 +0300

    updated fzf_writer config

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -1,56 +1,38 @@
 return {
   "nvim-telescope/telescope.nvim",
-  dependencies = {
-    { "nvim-telescope/telescope-fzf-native.nvim", enabled = vim.fn.executable "make" == 1, build = "make" },
-    { "nvim-telescope/telescope-fzf-writer.nvim" },
+  dependencies = { -- add a new dependency to telescope that is our new plugin
+    "nvim-telescope/telescope-fzf-writer.nvim",
   },
-  cmd = "Telescope",
-  opts = function()
-    local actions = require "telescope.actions"
-    local get_icon = require("astronvim.utils").get_icon
+  -- the first parameter is the plugin specification
+  -- the second is the table of options as set up in Lazy with the `opts` key
+  config = function(plugin, opts)
+    -- run the core AstroNvim configuration function with the options table
+    require("plugins.configs.telescope")(plugin, opts)
 
-    require('telescope').setup {
+    -- require telescope and load extensions as necessary
+    local telescope = require "telescope"
+
+    telescope.setup({
       extensions = {
+        fzf = {
+          fuzzy = true,                   -- false will only do exact matching
+          override_generic_sorter = true, -- override the generic sorter
+          override_file_sorter = true,    -- override the file sorter
+          case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+          -- the default case_mode is "smart_case"
+        },
         fzf_writer = {
-          minimum_grep_characters = 20,
-          minimum_files_characters = 20,
+          minimum_grep_characters = 3,
+          minimum_files_characters = 3,
           -- Disabled by default.
           -- Will probably slow down some aspects of the sorter, but can make color highlights.
           -- I will work on this more later.
           use_highlighter = true,
         }
       }
-    }
+    })
 
-    return {
-      defaults = {
-        prompt_prefix = string.format("%s ", get_icon "Search"),
-        selection_caret = string.format("%s ", get_icon "Selected"),
-        path_display = { "truncate" },
-        sorting_strategy = "ascending",
-        layout_config = {
-          horizontal = {
-            prompt_position = "top",
-            preview_width = 0.55,
-          },
-          vertical = {
-            mirror = false,
-          },
-          width = 0.87,
-          height = 0.80,
-          preview_cutoff = 120,
-        },
-        mappings = {
-          i = {
-            ["<C-n>"] = actions.cycle_history_next,
-            ["<C-p>"] = actions.cycle_history_prev,
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-k>"] = actions.move_selection_previous,
-          },
-          n = { ["q"] = actions.close },
-        },
-      },
-    }
+    -- telescope.load_extension "fzf"
+    telescope.load_extension "fzf_writer"
   end,
-  config = require "plugins.configs.telescope",
 }
